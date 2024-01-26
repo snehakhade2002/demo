ce2cfaa89b5f886d3e09d3611c75d81288b0f066
Sneha Khade <snehavirendrasahu1423@gmail.com>
2024-01-26 17:28:16 +0530

    added hungry promt

--- hungry.java
+++ hungry.java
@@ -1,8 +1,20 @@
-
+import java.util.*;
 
 public class hungry{
     public static void main(String[] args)
     {
-        System.out.println("hungry.......");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" are you hungry....??");
+        String ipstr = sc.nextLine();
+        ipstr = ipstr.toLowerCase().trim();
+        if(ipstr.equals("yes"))
+        {
+            System.out.println(" eat an apple");
+        }
+        else
+        {
+            System.out.println(" code.......");
+        }
     }
 }
